4b20b589fdf08e299266433b130024dcde18f780
Eddie <eddie.ch.89d@gmail.com>
2021-01-11 16:22:17 +0100

    Application setup

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -13,17 +13,42 @@ import java.lang.Math.*;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Vending Machine");
+        System.out.println(" ##### Vending Machine ##### ");
         VendingMachine vm = new VendingMachineImpl();
         Scanner scanner = new Scanner(System.in);
-        int user = 0;
-        int answer = 0;
+
+        System.out.println(" Enter a valid operation: ");
+        String operationType = "";
 
         while(true) {
-            System.out.println("Generate");
+            System.out.println(" Type e to exit ");
+            operationType = scanner.nextLine();
+            System.out.println(operationType);
 
-            user = scanner.nextInt();
-            System.out.println(answer);
+            switch (operationType) {
+                case "1":
+                    // todo:
+                    System.out.printf("a");
+                    break;
+                case "2":
+                    // todo:
+                    System.out.printf("b");
+                    break;
+                case "3":
+                    // todo:
+                    System.out.printf("c");
+                    break;
+                case "4":
+                    // todo:
+                    System.out.printf("d");
+                    break;
+                case "e":
+                    // todo: call exist method or stop application
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println(" operation is not valid ");
+            }
         }
     }
 
