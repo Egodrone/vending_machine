0219e7951e60bcaeb63bdbbdca3006dcfe837d35
Eddie <eddie.ch.89d@gmail.com>
2021-01-11 16:12:41 +0100

    Test with Junit setup

---
+++ src/test/java/se/lexicon/data/VendingMachineImpl.java
@@ -0,0 +1,12 @@
+package se.lexicon.data;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class VendingMachineImpl {
+
+}
---
+++ src/test/java/se/lexicon/model/Snack.java
@@ -0,0 +1,13 @@
+package se.lexicon.model;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class Snack {
+
+}
