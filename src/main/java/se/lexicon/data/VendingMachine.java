fae89b07a0212665505088c0bac518ecb893d4d2
Eddie <eddie.ch.89d@gmail.com>
2021-01-11 13:33:03 +0100

    data package setup

--- src/main/java/se/lexicon/data/VendingMachine.java
+++ src/main/java/se/lexicon/data/VendingMachine.java
@@ -1,9 +1,24 @@
 package se.lexicon.data;
 
 
+import se.lexicon.model.Product;
 
 public interface VendingMachine {
+
+    boolean addMoney(int money);
+
+    Product buy(int money, int prodNum);
+
+    int returnChange();
+
+    void presentProducts();
+
+    int getBalance();
+
+
+
+    /*
     default void addCurrency(int amount){
 
-    }
+    }*/
 }
--- src/main/java/se/lexicon/data/VendingMachineImpl.java
+++ src/main/java/se/lexicon/data/VendingMachineImpl.java
@@ -2,8 +2,35 @@ package se.lexicon.data;
 
 
 
-public class VendingMachineImpl {
+import se.lexicon.model.Product;
 
 
 
+public class VendingMachineImpl implements VendingMachine {
+
+
+    @Override
+    public boolean addMoney(int money) {
+        return false;
+    }
+
+    @Override
+    public Product buy(int money, int prodNum) {
+        return null;
+    }
+
+    @Override
+    public int returnChange() {
+        return 0;
+    }
+
+    @Override
+    public void presentProducts() {
+
+    }
+
+    @Override
+    public int getBalance() {
+        return 0;
+    }
 }
