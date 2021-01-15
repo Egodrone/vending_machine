c0ce11382ad333f56e8d62358ef8ba98719ae761
Eddie <eddie.ch.89d@gmail.com>
2021-01-15 17:44:28 +0100

    Food obj test with Junit

--- src/test/java/se/lexicon/model/FoodTest.java
+++ src/test/java/se/lexicon/model/FoodTest.java
@@ -2,7 +2,55 @@ package se.lexicon.model;
 
 
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
 public class FoodTest {
+
+    @Test
+    public void test_name() {
+        Food food = new Food("Chicken", 50, 400);
+        String expected = "Chicken";
+        assertEquals(expected, food.getName());
+    }
+
+
+
+    @Test
+    public void test_price() {
+        Food food2 = new Food("Chicken", 50, 400);
+        int expected = 50;
+        assertEquals(expected, food2.getPrice());
+    }
+
+
+    @Test
+    public void test_calories() {
+        Food food3 = new Food("Chicken", 50, 400);
+        int expected = 50;
+        assertEquals(expected, food3.getPrice());
+    }
+
+
+
+    @Test
+    public void test_use() {
+        Food food4 = new Food("Chicken", 50, 400);
+        String expected = " Have a nice meal eating " + "Chicken";
+        assertEquals(expected, food4.use());
+    }
+
+
+
+    @Test
+    public void test_examine() {
+        Food food5 = new Food("Chicken", 50, 400);
+        String expected = "Chicken" + " " + 50;
+        assertEquals(expected, food5.examine());
+    }
+
 
 
 }
