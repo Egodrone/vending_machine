9b7f69d873a4b692b12d2ca1df60c05177da3a20
Eddie <eddie.ch.89d@gmail.com>
2021-01-15 17:50:48 +0100

    Sequencer test with Junit

--- src/test/java/se/lexicon/model/SequencerTest.java
+++ src/test/java/se/lexicon/model/SequencerTest.java
@@ -2,7 +2,27 @@ package se.lexicon.model;
 
 
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
 public class SequencerTest {
+
+
+
+    @Test
+    public void testSequencer() {
+        int expected = 30;
+        int test = 0;
+        int counter = 0;
+        while (counter < 30) {
+            test = Sequencer.nextProductId();
+            ++counter;
+        }
+        assertEquals(expected, test);
+    }
+
 
 
 }
